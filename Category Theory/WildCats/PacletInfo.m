ebd2c2e682c2f86a1739cb96bf6bb34d19c9ccf3
nzmax <631025692@qq.com>
2013-12-27 12:55:05 +0800

    Organize files

---
+++ Category Theory/WildCats/PacletInfo.m
@@ -0,0 +1,17 @@
+(* Paclet Info File *)
+
+(* created 2013/05/02*)
+
+Paclet[
+    Name -> "WildCats",
+    Version -> "0.62.1",
+    MathematicaVersion -> "8+",
+    Description -> "Category Theory with Mathematica",
+    Creator -> "Alessandro Stecchina",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English", MainPage -> "Guides/WildCats"}
+        }
+]
+
+
